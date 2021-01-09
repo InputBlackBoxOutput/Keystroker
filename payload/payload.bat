7c9983e852477a78d5a199e6be1e6bbf360b76fb
Rutuparn <rutuparn.17U253@viit.ac.in>
2021-01-09 20:55:16 +0530

    Payload added

---
+++ payload/payload.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO I have messed up with your computer!
+ECHO.
+ping localhost -n 3
+REM SHUTDOWN -s -t 10 -c "Critical sytem error occurred :0x0008FF80"
+PAUSE
---
+++ payload/payload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo I have messed up with your computer!
+ping localhost -n 3
+rem sudo shutdown +1 "Critical sytem error occurred :0x0008FF80"
+pause
